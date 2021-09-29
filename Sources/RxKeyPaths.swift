c1ef86ba93db3253c9ebc2f70c7a0c1087273d3c
Weston Mitchell <weston@magma-media.com>
2021-09-29 16:50:00 -0700

    save @available(iOS 11.0, *) update

--- Sources/RxKeyPaths.swift
+++ Sources/RxKeyPaths.swift
@@ -9,16 +9,21 @@
 import CoreLocation.CLLocationManager
 
 extension String {
+    @available(iOS 11.0, *)
     static let activityType = #keyPath(CLLocationManager.activityType)
+    @available(iOS 11.0, *)
     static let distanceFilter = #keyPath(CLLocationManager.distanceFilter)
     static let desiredAccuracy = #keyPath(CLLocationManager.desiredAccuracy)
+    @available(iOS 11.0, *)
     static let pausesLocationUpdatesAutomatically = #keyPath(CLLocationManager.pausesLocationUpdatesAutomatically)
     static let allowsBackgroundLocationUpdates = #keyPath(CLLocationManager.allowsBackgroundLocationUpdates)
+    @available(iOS 11.0, *)
     static let showsBackgroundLocationIndicator = #keyPath(CLLocationManager.showsBackgroundLocationIndicator)
     static let location = #keyPath(CLLocationManager.location)
     static let headingFilter = #keyPath(CLLocationManager.headingFilter)
     static let headingOrientation = #keyPath(CLLocationManager.headingOrientation)
     static let heading = #keyPath(CLLocationManager.heading)
+    @available(iOS 11.0, *)
     static let maximumRegionMonitoringDistance = #keyPath(CLLocationManager.maximumRegionMonitoringDistance)
     static let monitoredRegions = #keyPath(CLLocationManager.monitoredRegions)
     static let rangedRegions = #keyPath(CLLocationManager.rangedRegions)
